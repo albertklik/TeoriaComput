9e7297df94e52ce0d7ce7d0d69a018a5f234c96f
AlanTas <alantas@outlook.com>
2017-12-20 20:07:39 -0300

    Adicionados os campos

---
+++ app/src/main/java/com/example/root/teoriacomput/CadastroMain.java
@@ -0,0 +1,13 @@
+package com.example.root.teoriacomput;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastroMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_main);
+    }
+}
